4171bb5423d7434e416889f8b7ba0bbb0d6d7ac5
André Costa <55462127+AndreCostaaa@users.noreply.github.com>
2021-12-05 21:06:53 +0100

    testing 14

--- transmission.py
+++ transmission.py
@@ -205,7 +205,7 @@ class Transmission:
                 index_str = ''.join(argument_lst)
                 index = int(index_str)
                 self.payload = cmd_detail.upper() + index_str + self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][index]
-                print(self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][index])
+                print(self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]])
 
                 #self.payload += self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][index]
             self.is_data_requested = True
