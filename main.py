5b5e1dba2aa4d5ae35398abeab2952880fe2015b
André Costa <55462127+AndreCostaaa@users.noreply.github.com>
2021-12-05 19:36:31 +0100

    testing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class main:
     def __init__(self):
         from wheel import Wheel
         self.wheel = Wheel()
-        self.transmission = Transmission()
+        self.transmission = Transmission(self.wheel)
         self.run()
 
     def run(self):
@@ -19,18 +19,7 @@ class main:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            if self.transmission.check_new_data():
-                data = self.transmission.read_data()
-                if not self.transmission.treat_data_in(data, self.wheel) < 0:
-                    self.transmission.send_ack()
-            
-            print(self.wheel.get_axes())
-            print(self.wheel.get_hats())
-            print(self.wheel.get_buttons())
-            time.sleep(.5)
-            
-            self.transmission.handle_transmission(pygame.time.get_ticks(), self.wheel)
-
+            self.transmission.handle_transmission()
 
 if __name__ == "__main__":
     pygame.init()
--- transmission.py
+++ transmission.py
@@ -30,101 +30,103 @@ MAX_FREQUENCY = 1000 # 10 ms
 
 class Transmission:
 
-    def __init__(self):    
-        self.state = S_WAITING
-        self.mode = M_AUTO
+    def __init__(self, wheel):    
+        self.state = S_TRANSMITTING
+        self.mode = M_ON_REQUEST
         self.type = T_EXPLICIT
         self.data_per_sec = 0
         self.time_between_comms = TIME_BETWEEN_DATA_TX_WHEN_WAITING_CONNECTION
         self.last_information_sent = 0
 
         self.payload_sent = False
-        self.data_was_requested = False
+        self.is_data_requested = False
         self.data_tram = []
 
-        self.serial = serial.Serial("COM4", 115200)
+        self.serial = serial.Serial("/dev/ttyUSB0", 115200)
         self.serial.flush()
-
+        self.wheel = wheel
         self.payload = WAITING_OCTET
 
-    def build_payload(self, wheel):
+    def _build_payload(self):
         payload = None
 
         if self.type == T_EXPLICIT:
             payload = ""
-            for key in wheel.explicit_data.keys():
-                for i in range(len(wheel.explicit_data[key])):
-                    payload += key[0].upper() + str(i) + str(wheel.explicit_data[key][i])
+            for key in self.wheel.explicit_data.keys():
+                for i in range(len(self.wheel.explicit_data[key])):
+                    payload += key[0].upper() + str(i) + str(self.wheel.explicit_data[key][i])
             
         if self.type == T_COMPACT:
             payload = bytearray()
-            for key in wheel.compact_data.keys():
-                for i in range(len(wheel.compact_data[key])):
-                    payload.append(wheel.compact_data[key][i])
+            for key in self.wheel.compact_data.keys():
+                for i in range(len(self.wheel.compact_data[key])):
+                    payload.append(self.wheel.compact_data[key][i])
         #payload += "\n"
         self.payload = payload
 
-    def handle_transmission(self, time_now, wheel):            
+    def handle_transmission(self, time_now):
+        if self._is_new_data():
+            self._treat_data_in(self._read_data())
+        send_payload = False            
         if self.state == S_WAITING and time_now - self.last_information_sent >= TIME_BETWEEN_DATA_TX_WHEN_WAITING_CONNECTION:
                 print("Waiting for Connection")
                 self.payload = WAITING_OCTET
-                self.send_payload()
+                send_payload = True
         elif self.state == S_TRANSMITTING:
             if self.mode == M_AUTO and (time_now - self.last_information_sent) >= int(self.time_between_comms):
                 if DEBUG:
                     print("Time since last tx: " + str(time_now - self.last_information_sent), end=" ")
-                self.build_payload(wheel)
-                self.send_payload()
-            elif self.mode == M_ON_REQUEST and self.data_was_requested:
-                self.send_payload()
-                self.data_was_requested = False
+                self._build_payload()
+                send_payload = True
 
-        if self.payload_sent:
-            self.payload_sent = False
+            elif self.mode == M_ON_REQUEST and self.is_data_requested:
+                send_payload = True
+                self.is_data_requested = False
+
+        if send_payload:
+            self._send_payload()
             self.last_information_sent = time_now
 
-    def send_payload(self):
+    def _send_payload(self):
         if type(self.payload) == str:
             self.payload = self.payload.encode('ascii')
         if DEBUG:
             print(f"Sending payload: {self.payload=}")
         self.serial.write(self.payload)
         self.serial.write(b"\r\n")
-        self.payload_sent = True
     
-    def check_new_data(self):
-        if self.serial.in_waiting > 0:
-            return True
-        return False
+    def _is_new_data(self):
+        return self.serial.in_waiting > 0
     
-    def read_data(self):
+    def _read_data(self):
         #waiting for the /n. Code will be blocked here for 1 sec max if we don't get it
         return self.serial.readline() 
     
-    def treat_data_in(self, data, wheel):
+    def _treat_data_in(self, data):
         
-        if not self.is_valid_start(data):
-            self.log_error("bad start")
+        if not self._is_valid_start(data):
+            self._log_error("bad start")
 
-        if not self.is_valid_end(data):
-            self.log_error("bad end")
+        if not self._is_valid_end(data):
+            self._log_error("bad end")
             return -1
 
-        if not self.is_valid_size(data):
-            self.log_error("missing args")
+        if not self._is_valid_size(data):
+            self._log_error("missing args")
             return -1
         
         cmd, cmd_detail = (chr(data[CMD_POSITION]).upper(), chr(data[CMD_DETAIL_POSITION]).upper())
 
         if not cmd in VALID_COMMANDS:
-            self.log_error("invalid cmd")
+            self._log_error("invalid cmd")
             return -1
 
         if not cmd_detail in VALID_COMMAND_DETAIL[cmd]:
-            self.log_error("invalid cmd detail")
+            self._log_error("invalid cmd detail")
             return -1
 
-        argument_lst = self.get_args(data)
+        argument_lst = self._get_args(data)
+        
         if cmd == SET:
             if cmd_detail == MODE:
                 if DEBUG:
@@ -138,7 +140,7 @@ class Transmission:
                     self.mode = M_ON_REQUEST
                     self.state = S_TRANSMITTING
                 else:
-                    self.log_error("invalid mode")
+                    self._log_error("invalid mode")
                     return -1
 
                 if DEBUG:
@@ -155,7 +157,7 @@ class Transmission:
                 elif arg == COMPACT:
                     self.type = T_COMPACT
                 else:
-                    self.log_error("invalid type")
+                    self._log_error("invalid type")
                     return -1
 
                 if DEBUG:
@@ -185,7 +187,6 @@ class Transmission:
 
                 if DEBUG:
                     print(f"to the frequency {freq} Hz\n{self.time_between_comms=}")
-
         elif cmd == GET and self.mode == M_ON_REQUEST:
 
             if argument_lst[0] == '9':
@@ -195,48 +196,46 @@ class Transmission:
 
                 self.payload = cmd_detail.upper() + ALL
                 
-                for i in range(len(wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]])):
-                    self.payload += str(wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][i])      
+                for i in range(len(self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]])):
+                    self.payload += str(self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][i])      
             else:                   
-                
                 index_str = ''.join(argument_lst)
                 index = int(index_str)
-                self.payload = cmd_detail.upper() = index_str
-                self.payload += str(wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][index])
+                self.payload = cmd_detail.upper() + index_str
+                self.payload += str(self.wheel.explicit_data[DATA_FROM_COMMANDS_DIC[cmd_detail]][index])
 
-            self.data_was_requested = True
- 
+            self.is_data_requested = True
         else:
-            self.log_error("mode is auto")
+            self._log_error("mode is auto")
             return -1
         return 0
 
     
-    def log_error(self, detail):
+    def _log_error(self, detail):
         data = "ERROR " + detail       
         if DEBUG:
             print(data)
         self.serial.write(NACK)
 
-    def send_ack(self):
+    def _send_ack(self):
         self.serial.write(ACK)
 
     @staticmethod
-    def is_valid_start(data):
+    def _is_valid_start(data):
         return data[0] == START_CHAR
 
     @staticmethod
-    def is_valid_end(data):
+    def _is_valid_end(data):
         return data[-1] == END_CHAR
     
     @staticmethod
-    def is_valid_size(data):
+    def _is_valid_size(data):
         return len(data) - 2 > 2
 
     @staticmethod
-    def get_argument_index(argument_number):
+    def _get_argument_index(argument_number):
         return FIRST_ARG_POSITION + argument_number
     
     @staticmethod
-    def get_args(data):
+    def _get_args(data):
         return data[FIRST_ARG_POSITION: -1]
